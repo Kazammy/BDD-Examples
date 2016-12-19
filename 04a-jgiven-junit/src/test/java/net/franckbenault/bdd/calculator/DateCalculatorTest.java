60e4267aa8d03705479872a5af6e25f6f99f1680
franck-benault <franck-benault@orange.fr>
2016-12-19 22:43:24 +0100

    add jgiven for junit subproject

---
+++ 04a-jgiven-junit/src/test/java/net/franckbenault/bdd/calculator/DateCalculatorTest.java
@@ -0,0 +1,22 @@
+package net.franckbenault.bdd.calculator;
+
+
+import org.junit.Test;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.junit.SimpleScenarioTest;
+import net.franckbenault.bdd.calculator.DateCalculatorTest.TestSteps;
+
+public class DateCalculatorTest extends SimpleScenarioTest<TestSteps> {
+
+	@Test
+	public void test() {
+	}
+
+	
+	public static class TestSteps extends Stage<TestSteps> {
+		
+		private int i=0;
+		
+	}
+}
