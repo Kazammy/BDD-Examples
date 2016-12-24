affd1deda4d1b18853d4d7fd53581683af7d5e54
franck-benault <franck-benault@orange.fr>
2016-12-24 18:20:02 +0100

    add html report with Cucumber

--- 01b-cucumber-testng/src/test/java/net/franckbenault/bdd/calculator/RunCukesTest.java
+++ 01b-cucumber-testng/src/test/java/net/franckbenault/bdd/calculator/RunCukesTest.java
@@ -3,6 +3,6 @@ package net.franckbenault.bdd.calculator;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(plugin = "json:target/cucumber-report.json")
+@CucumberOptions(plugin = {"json:target/cucumber-report.json","html:target/cucumber"})
 public class RunCukesTest extends AbstractTestNGCucumberTests {
 }
